97dea2f098d05e3d698cc713d6d57bc2ef2a8818
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-01-29 16:05:30 +0100

    Calculate neighbor indices without function neighborIndex2offset

--- MATLAB/mass_spring_damper2D_algorithm.m
+++ MATLAB/mass_spring_damper2D_algorithm.m
@@ -1,29 +1,58 @@
 % Allokering av minne
-N = [5 4];
-masses = ones(N);
-spring_constants = 200*ones(N-1);
-spring_length = 4*ones(N-1);
-damper_constants = 6*ones(N-1);
+n = 5;
+m = 4;
+SIZE = [n m];
+number_of_masses = prod(SIZE);
+
+masses = ones(number_of_masses,1);
+
+%number of connections:    -        |         / and \
+number_of_connections = (n-1)*m + n*(m-1) + 2*(n-1)*(m-1);
+
+spring_constants = 200*ones(SIZE-1);
+spring_length = 4*ones(SIZE-1);
+damper_constants = 6*ones(SIZE-1);
 
 
-positions = zeros([N 2]);
-velocities = zeros([N 2]);
+positions = zeros([SIZE 2]);
+velocities = zeros([SIZE 2]);
 
 
 % skapa lista med index till grannar
 max_number_of_neighbors = 8;
-neighbor_index = zeros([N max_number_of_neighbors]);
+neighbor_index = zeros([SIZE max_number_of_neighbors]);
 
 % todo: 
-% fixa indexlistor till 
-% grannar och till connections
+% fixa linjära indexlistor till 
+% till connections
 
-i=1;
-for y=1:N(1)
-    for x=1:N(2)
-        if y<=1
+connection_index = zeros(prod(SIZE), max_number_of_neighbors);
+neighbor_index = zeros(prod(SIZE), max_number_of_neighbors);
+
+%% Calculate neighbor indices
+for j=1:N % For each mass
+    
+    %Compute cartesian index
+    [y, x] = ind2sub(SIZE,j); 
+    
+    for z=1:max_number_of_neighbors % For each neighbor
+        
+        % Calculate cartesian index
+        neighbor_pos = [y, x] + neighborIndex2offset(z); 
+        
+        % Store y and x index as nice variables
+        y_ = neighbor_pos(1); 
+        x_ = neighbor_pos(2);
+        
+        %Check if neighbor exist
+        if 0<y_ && y_<=n  &&  0<x_ && x_<=m
+            % Add linear index in neighbor index list
+            neighbor_index(j,z) = sub2ind(SIZE,y_,x_);
+        end
     end
 end
+
+%% Calculate connection indices
 
 
 
@@ -35,10 +64,10 @@ close all;
 read_buffer_index = 1;
 write_buffer_index = 2;
 
-positions(:,1) = 4*(1:N);
-velocities(:,1) = zeros(1,N);
+positions(:,1) = 4*(1:SIZE);
+velocities(:,1) = zeros(1,SIZE);
 
-positions(2:N,1) = positions(2:N)-1;
+positions(2:SIZE,1) = positions(2:SIZE)-1;
 
 %positions(1,1) = -1;
 
@@ -49,7 +78,7 @@ t = T*(0:n_frames-1);
 
 for i=1:n_frames %Loop through frames
     t=t+T;
-    for j=1:N %Loop through masses
+    for j=1:SIZE %Loop through masses
         
         positions(1,1)=1;
         
@@ -85,7 +114,7 @@ for i=1:n_frames %Loop through frames
     end
     
     
-    plot(ones(1,N),positions(:,write_buffer_index),'*');
+    plot(ones(1,SIZE),positions(:,write_buffer_index),'*');
     hold off;
     axis manual;
     axis([0 2 -1 31]);
