7c38437485d247c164cbe29387f52221567c9c66
kbladin <kallebladin@gmail.com>
2014-01-29 15:46:46 +0100

    added *.m~ to .gitignore and created file neighborIndex2offset.m which will be a matlab function.

---
+++ MATLAB/neighborIndex2offset.m
@@ -0,0 +1,10 @@
+% Insert an index and get the offset to the neighbor
+% in both directions.
+% In 2D, the indices have the following configuration: 
+% 8 1 2
+%  \|/
+% 7-o-3
+%  /|\
+% 6 5 4
+
+function neighborIndex2offset()
