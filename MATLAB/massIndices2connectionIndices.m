d1f1d2949c29a8ed59f83ac2d74f60ef0c73a424
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-01-30 15:06:16 +0100

    Added massIndices2connectionIndices, tested and working

---
+++ MATLAB/massIndices2connectionIndices.m
@@ -0,0 +1,43 @@
+% input: i = index to one mass
+% input: j = index to neighbor mass
+% input: nCols = number of columns
+% output: d = direction, used for connection list index
+% output: c = connection index
+function [d, c] = massIndices2connectionIndices(i,j,nCols)
+d = -1;
+c = -1;
+    switch abs(i-j)
+        
+        % connection orientation: -
+        case 1 
+            if rem(min(i,j),nCols) ~= 0
+                d = 3;
+            else
+                return
+            end
+        
+        % connection orientation: /
+        case nCols-1 
+            if rem(min(i,j),nCols) ~= 0
+                d = 2;
+            else
+                return
+            end
+            
+        % connection orientation: |
+        case nCols 
+            d = 1;
+            
+        % connection orientation: \
+        case nCols+1 
+            if rem(min(i,j),nCols) ~= 0
+                d = 4;
+            else
+                return
+            end
+            
+        otherwise
+            error(['|i-j| = ', num2str(abs(i-j))]);
+    end
+c = min(i,j);
+end
