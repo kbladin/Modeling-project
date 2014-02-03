048a802582f13faf356f4ddce2bf29ca40c2fd22
kbladin <kallebladin@gmail.com>
2014-02-03 14:36:44 +0100

    Added gravity and simple collision with ground.

--- MATLAB/mass_spring_damper2D_algorithm.m
+++ MATLAB/mass_spring_damper2D_algorithm.m
@@ -1,7 +1,7 @@
 % Allokering av minne
 clear all;
 nRows = 4;
-nCols = 3;
+nCols = 4;
 SIZE = [nRows nCols];
 
 g = 9.82;
@@ -20,7 +20,7 @@ for j=1:number_of_masses % For each mass
     positions(j,:,1) = [floor((j-1)/nCols), rem(j-1,nCols)];
 end
 
-positions(1,:,1) = [0, -0.5];
+%positions(1,:,1) = [0, -0.5];
 
 
 % Create list with indices to neighbors and connections
@@ -38,12 +38,12 @@ for j=1:number_of_masses % For each mass
 end
 
 %--Connections list--
-spring_constants = 100000 * ones(number_of_connections, 1);
-damper_constants = 50 * ones(number_of_connections, 1);
+spring_constants = 2000 * ones(number_of_connections, 1);
+damper_constants = 5 * ones(number_of_connections, 1);
 spring_length = ones(number_of_connections, 1);
 
-%spring_length(nRows*(nCols-1)+1:nRows*(nCols-1) + (nRows-1)*(nCols-1)) = sqrt(2);
-%spring_length(nRows*(nCols-1) + (nRows-1)*(nCols-1) + (nRows-1)*nCols + 1:number_of_connections) = sqrt(2);
+spring_length(nRows*(nCols-1)+1:nRows*(nCols-1) + (nRows-1)*(nCols-1)) = sqrt(2);
+spring_length(nRows*(nCols-1) + (nRows-1)*(nCols-1) + (nRows-1)*nCols + 1:number_of_connections) = sqrt(2);
 
 
 close all;
@@ -54,23 +54,23 @@ write_buffer_index = 2;
 
 velocities(:,:,:) = 0;
 for j=1:number_of_masses % For each mass
-    positions(j,:,read_buffer_index) = [floor((j-1)/nCols), rem(j-1,nCols)];
+    positions(j,:,read_buffer_index) = [15 + floor((j-1)/nCols), rem(j-1,nCols)];
 end
 %positions(1,:,read_buffer_index) = [0, -1];
-%velocities(1,:,read_buffer_index) = [-100, -100];
+velocities(1,:,read_buffer_index) = [5, -5];
+velocities(number_of_masses,:,read_buffer_index) = [-5, 5];
 
 n_frames = 5000;
-T = 0.001;
+T = 0.01;
 
 figure;
 %pause(2);
 %hold on;
 for i=1:n_frames %Loop through frames
-    
-    tic;
+	tic;
+    %tic;
     for j=1:number_of_masses %Loop through masses        
         F = [0,0];
-        
         for dir=1:max_number_of_neighbors %Loop through neighbors in all directions
             con_ind = connection_index(j,dir);
             neigh_ind = neighbor_index(j,dir);
@@ -108,10 +108,20 @@ for i=1:n_frames %Loop through frames
                 F = F + (-k*(norm(deltaP) - l) - b*dot(deltaV, deltaP_hat))*deltaP_hat;
             end
         end
+        
+        %Add the Gravity force
+        F = F + masses(j)*g*[-1,0];
+        
         %Calculacte acceleration, velocity and position
         a = F/masses(j);
         v = velocities(j, :,read_buffer_index) + T*a;
         p = positions(j, :,read_buffer_index) + T*v;
+        
+        %Check collision with y=0
+        if p(1) < 0
+            p(1) = 0;
+            v(1) = -v(1); 
+        end
         
         %Store information in backbuffer
         velocities(j, :,write_buffer_index) = v;
@@ -120,12 +130,12 @@ for i=1:n_frames %Loop through frames
     plot(positions(:,2,write_buffer_index), ...
             positions(:,1,write_buffer_index),'*');
     axis manual;
-    axis([-2 5 -2 5]);
-    pause(T);
+    axis([-2 5 0 20]);
+    pause(max(T-toc,0.001));
     
     %Swap buffer
     read_buffer_index = rem(read_buffer_index,2)+1;
     write_buffer_index = rem(write_buffer_index,2)+1;
     %clf('reset');
-    toc
+    %toc;
 end
