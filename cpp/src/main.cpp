cb463527c1c431f32c558fe9d6c0ebd5cbb52d6c
kbladin <kallebladin@gmail.com>
2014-02-07 10:10:22 +0100

    In the middle of trying to go to new openGL code in the main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,6 +1,8 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <glm/glm.hpp>
+
 
 static void error_callback(int error, const char* description)
 {
@@ -11,6 +13,8 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+/*
 int main(void)
 {
     GLFWwindow* window;
@@ -53,4 +57,175 @@ int main(void)
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+*/
+
+
+
+
+ 
+void myInitFun();
+void myDrawFun();
+void myPreSyncFun();
+void myCleanUpFun();
+ 
+//global vars
+GLuint vertexArray = GL_FALSE;
+GLuint vertexPositionBuffer = GL_FALSE;
+GLuint vertexColorBuffer = GL_FALSE;
+
+GLuint programID;
+
+GLint Matrix_Loc = -1;
+ 
+int main( int argc, char* argv[] )
+{
+    // Init
+    myInitFun();
+ 
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        myDrawFun();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    myCleanUpFun();
+     
+    // Exit program
+}
+ 
+void myInitFun()
+{
+    // Init glfw
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+    
+
+    // Data
+    const GLfloat vertex_position_data[] = { 
+        -0.5f, -0.5f, 0.0f,
+         0.0f, 0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f
+    };
+ 
+    const GLfloat vertex_color_data[] = { 
+        1.0f, 0.0f, 0.0f, //red
+        0.0f, 1.0f, 0.0f, //green
+        0.0f, 0.0f, 1.0f //blue
+    };
+ 
+    //generate the VAO
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+ 
+    //generate VBO for vertex positions
+    glGenBuffers(1, &vertexPositionBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexPositionBuffer);
+    //upload data to GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_position_data), vertex_position_data, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        reinterpret_cast<void*>(0) // array buffer offset
+    );
+ 
+    //generate VBO for vertex colors
+    glGenBuffers(1, &vertexColorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexColorBuffer);
+    //upload data to GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_color_data), vertex_color_data, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(
+        1,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        reinterpret_cast<void*>(0) // array buffer offset
+    );
+ 
+    glBindVertexArray(0); //unbind
+ 
+
+    //ADD SHADER HERE
+    programID = LoadShaders( "data/shaders/simple.vert", "data/shaders/simple.frag" );
+ 
+    //BIND SHADER HERE
+    //glUseProgram(programID);
+ 
+    //GET UNIFORM LOCATION FOR MVP MATRIX HERE
+    //Matrix_Loc = sgct::ShaderManager::instance()->getShaderProgram( "xform").getUniformLocation( "MVP" );
+
+    //UNBIND SHADER HERE
+    // ------ 
+}
+ 
+void myDrawFun()
+{
+    glUseProgram(programID);
+    float speed = 50.0f;
+ 
+    glm::mat4 scene_mat = glm::rotate( glm::mat4(1.0f), static_cast<float>( curr_time.getVal() ) * speed, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 MVP = gEngine->getActiveModelViewProjectionMatrix() * scene_mat;
+ 
+    //BIND SHADER HERE
+    // ------
+ 
+    glUniformMatrix4fv(Matrix_Loc, 1, GL_FALSE, &MVP[0][0]);
+ 
+    glBindVertexArray(vertexArray);
+ 
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+ 
+    //unbind
+    glBindVertexArray(0);
+    
+    //UNBIND SHADER HERE
+    glUseProgram(0);
+}
+ 
+void myPreSyncFun()
+{
+
+}
+ 
+void myCleanUpFun()
+{
+    // Terminate glfw
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
+    // Release memory
+    if(vertexPositionBuffer)
+        glDeleteBuffers(1, &vertexPositionBuffer);
+    if(vertexColorBuffer)
+        glDeleteBuffers(1, &vertexColorBuffer);
+    if(vertexArray)
+        glDeleteVertexArrays(1, &vertexArray);
 }
