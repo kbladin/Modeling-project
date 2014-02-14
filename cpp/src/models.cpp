11d766bcac3bb2dff6f16335d12e6cbfc3e6ff07
Emma Forsling Parborg <Emma@c-67-132.eduroam.liu.se>
2014-02-14 12:24:23 +0100

    Local commit

--- cpp/src/models.cpp
+++ cpp/src/models.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 #include <glm/glm.hpp>
 //#include <models.h>
+
+class Particle
+{
+public:
+    Particle(const float mass = 1, 
+             const glm::vec3& position = glm::vec3(0,0,0),
+             const glm::vec3& velocity = glm::vec3(0,0,0));
+    ~Particle();
+    
+    const glm::vec3& readPosition() const;
+    const glm::vec3& readVelocity() const;
+
+    void writePosition(const glm::vec3& p);
+    void writeVelocity(const glm::vec3& v);
+
+    static void swapBuffers();
+
+private:
+
+    float _mass;
+    glm::vec3 _position[2];
+    glm::vec3 _velocity[2];
+    glm::vec3 _force;
+
+    static int read_buffer_index;
+    static int write_buffer_index;
+
+};
+
+
+
+
 // The abstract subclass MCS - Mass Connection System
 class MCS /*: public Model*/{
 
@@ -48,12 +80,12 @@ MCS::MCS(const int n_rows, const int n_cols, const int n_stacks)
     */
     particles = new Particle[N_PARTICLES];
 	setStartingValues();
-    connected_particles = new Particle[N_PARTICLES][2];
+/*    connected_particles = new Particle[N_PARTICLES][2];
 
 	// Calculate connections
     for (int i = 0; i < N_CONNECTIONS; ++i){
         connection2massIndices(i, connected_particles[i][0], connected_particles[i][1], N_ROWS, N_COLS, N_STACKS);
-    }
+    }*/
 
 }
 
