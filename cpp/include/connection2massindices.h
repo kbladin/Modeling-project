f2e15e200a9518aa3fbea7618c2b2a380159eb5b
kbladin <kallebladin@gmail.com>
2014-02-16 19:51:59 +0100

    Function connection2massindices3D now working in 2D :D. It is now used for the indexing. Added the ability to change the number of simulations per frame. This way, higher spring constants are possible.

--- cpp/include/connection2massindices.h
+++ cpp/include/connection2massindices.h
@@ -1,7 +1,16 @@
-
 #ifndef CONNECTION_2_MASSINDICES_
 #define CONNECTION_2_MASSINDICES_
 
+#include <assert.h> 
+#include <iostream>
+#include "glm/glm.hpp"
+
 void connection2massIndices(const int connection_index, int &mass_index1, int &mass_index2, const int n_rows, const int n_cols);
+
+void connection2massIndices3D(const int connection_index, int &mass_index1, int &mass_index2, const int n_rows, const int n_cols, const int n_stacks);
+
+int stackOfFirstMass(const int connection_index, const int prev_num_springs, const int one_stack_of_connections);
+
+int rowOfFirstMass(const int connection_index, const int prev_num_springs, const int one_row_of_connections, const int one_stack_of_connections);
 
 #endif
