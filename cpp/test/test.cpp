b8718aa40a6b64ea1efb77400b2a66dff6fdd33d
kbladin <kallebladin@gmail.com>
2014-02-20 10:01:42 +0100

    Added cmake shit and removed old makefile. Also added google test.

---
+++ cpp/test/test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "test.h"
+
+/* *
+* Test class for Chromosome
+*/
+class EMTest : public ::testing::Test {
+protected:
+	EMTest() {
+
+	}
+
+	virtual ~EMTest() {
+
+	}
+
+	virtual void SetUp() {
+
+	}
+
+	virtual void TearDown() {
+
+	}
+	
+	//Chromosome c;
+};
+
+TEST_F(EMTest, superMegaTestet) {
+	int right = 0;
+	std::cout << "Running test function and shit." << std::endl;
+	EXPECT_EQ(0,right);
+}
+
+TEST_F(EMTest, ParticleConnectionMCS) {
+	testParticle();
+    testConnection();
+    testMCS();
+}
