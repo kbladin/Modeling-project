a2167940cf0efb3dbda95a387a1fed82ba61c03a
kbladin <kallebladin@gmail.com>
2014-02-21 11:32:30 +0100

    Started to refoactor.

--- cpp/include/MCS.h
+++ cpp/include/MCS.h
@@ -38,11 +38,24 @@ class MCS{
         std::vector<glm::vec3> particlePositions;
         std::vector<glm::vec3> particleVelocities;
         std::vector<glm::vec3> particleAccelerations;
+        std::vector<glm::vec3> particleForces;
         std::vector<glm::vec3> particleMasses;
 
-        std::vector<glm::vec3> connectionLengths;
-        std::vector<glm::vec3> connectionSpringConstants;
-        std::vector<glm::vec3> connectionDamperConstants;
+        struct Particles{
+            std::vector<glm::vec3> positions;
+            std::vector<glm::vec3> velocities;
+            std::vector<glm::vec3> accelerations;
+            std::vector<glm::vec3> forces;
+            std::vector<glm::vec3> masses;
+        };
+
+        struct Connections{
+            std::vector<float> lengths;
+            std::vector<float> springConstants;
+            std::vector<float> damperConstants;    
+        };
+
+        
         
         //The number of connections of each type/direction
         int numberOfConnectionsOfType[13];
@@ -56,6 +69,14 @@ class MCS{
     protected:
         friend void testMCS();
 
+
+        void connection2massIndices3D(const int connection_index, int &mass_index1, int &mass_index2, const int n_rows, const int n_cols, const int n_stacks);
+
+        // Calculate the stack in which the first mass (not necessary mass 1) of the pair is positioned
+        int stackOfFirstMass(const int connection_index, const int prev_num_springs, const int one_stack_of_connections);
+
+        // Calculate the row in which first mass (not necessary mass 1) of the pair is positioned
+        int rowOfFirstMass(const int connection_index, const int prev_num_springs, const int one_row_of_connections, const int one_stack_of_connections);
         //void initParticles();
         //void initConnections();
         
